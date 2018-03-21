ea95aceb556a1cb798ddc8b004847d31bc7d0a5f
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-03-21 23:43:03 +0900

    タップされた座標にflowerを追加

---
+++ Animation/Animation/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by home on 2018/03/21.
+//  Copyright © 2018年 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+        // flowerを作る
+        let flower = UIImageView(image: UIImage(named: "flower"))
+        // 透明度を0にする
+        flower.alpha = 0
+    }
+}
+
